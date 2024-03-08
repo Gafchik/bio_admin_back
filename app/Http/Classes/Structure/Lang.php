49406381d09211306cabaac308ffb50a8bf63509
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2024-03-08 22:28:56 +0200

    init commit 2

---
+++ app/Http/Classes/Structure/Lang.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Classes\Structure;
+
+final class Lang
+{
+    public const RUS = 'ru';
+    public const UKR = 'uk';
+    public const ENG = 'en';
+    public const GEO = 'ge';
+
+    public const ARRAY_LANG = [
+        self::RUS,
+        self::UKR,
+        self::ENG,
+        self::GEO,
+    ];
+}
